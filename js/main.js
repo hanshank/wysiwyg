156c431981fb3b2b33261b7b43114ecafb87dd8a
Kevin Baktiar <afterafx@gmail.com>
2019-02-19 11:23:16 -0800

    finished feature 1 closes#1

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ const editButtons = document.querySelectorAll('.edit-button, .save-button');
 // add event listener (click)
 editButtons.forEach(button => button.addEventListener('click', toggleActive));
 
+// Listen for 'input' events form editor when fired
+document.querySelectorAll('.editor').forEach
+(editor => editor.addEventListener('input', handleEditorInput));
+
+// update text ( or style) of siblings
+function handleEditorInput(event) {
+  if (event.target.type === 'text' || event.target.tagName === 'TEXTAREA'){
+  const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+  const text = event.target.value;
+  textElement.innerText = text;
+  }
+}
+
