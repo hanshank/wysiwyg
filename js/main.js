eab01c4358bd4e5fcdf0b7d0ce5d3d08b20c3c3e
Kevin Baktiar <afterafx@gmail.com>
2019-02-19 10:06:31 -0800

    added toggle to edit button closes#5

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import toggleActive from './toggleActive.js';
+
+// select edit button
+const editButtons = document.querySelectorAll('.edit-button');
+// add event listener (click)
+editButtons.forEach(button => button.addEventListener('click', toggleActive));
+
