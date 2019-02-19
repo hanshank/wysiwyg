ce4a78112ae5663aaf7b4b431c060437678f3bfd
Kevin Baktiar <afterafx@gmail.com>
2019-02-19 10:52:08 -0800

    added save functionality closes#4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import toggleActive from './toggleActive.js';
 
 // select edit button
-const editButtons = document.querySelectorAll('.edit-button');
+const editButtons = document.querySelectorAll('.edit-button, .save-button');
 // add event listener (click)
 editButtons.forEach(button => button.addEventListener('click', toggleActive));
 
