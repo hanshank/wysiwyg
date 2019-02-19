86c7b60c03b50b7157f5795532855602ea025507
Kevin Baktiar <afterafx@gmail.com>
2019-02-19 11:27:09 -0800

    modularized function

---
+++ js/handleEditorInput.js
@@ -0,0 +1,8 @@
+// update text ( or style) of siblings
+export default function handleEditorInput(event) {
+  if (event.target.type === 'text' || event.target.tagName === 'TEXTAREA'){
+  const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+  const text = event.target.value;
+  textElement.innerText = text;
+  }
+}
