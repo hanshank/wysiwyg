37f32e618d6151eb8218da953bec5557840f4be5
hanshank <hans@hanken.org>
2019-02-19 11:29:31 -0800

    Add text edit functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import toggleActive from './toggleActive.js';
+import handleEditorInput from './handleEditorInput.js'
 
 // select edit buttons and call toggle function on click
 document.querySelectorAll('.edit-button, .save-button').forEach(button => button.addEventListener('click', toggleActive));
+
+// listen for 'input' events from editor
+document.querySelectorAll('.editor').forEach(editor => editor.addEventListener('input', handleEditorInput));
