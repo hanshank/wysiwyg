fb86ee0e5a1fbf23fa56cb6356e6ae703d2cfd07
hanshank <hans@hanken.org>
2019-02-19 12:07:08 -0800

    Add style functionality to edior configs

--- js/handleEditorInput.js
+++ js/handleEditorInput.js
@@ -1,8 +1,14 @@
 // when fired, update text (or style)
 export default function handleEditorInput(event) {
+  const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+
   if (event.target.type === 'text' || event.target.tagName === 'TEXTAREA')  {
-    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
     const text = event.target.value;
     textElement.innerText = text;
+  } else {
+    // handle the styles
+    const { value, name: styleName } = event.target;
+    const unit = event.target.dataset.unit || '';
+    textElement.style[styleName] = `${value}${unit}`;
   }
 } 
